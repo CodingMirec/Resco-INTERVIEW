33d481c7b51f1f805ba30de6362528ece469da2f
CodingMirec <m.sykora01@gmail.com>
2021-12-12 23:39:47 +0100

    setting up the basics

---
+++ client/src/pages/App.js
@@ -0,0 +1,12 @@
+import Phone from "../components/phone/Phone";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <Phone />
+    </div>
+  );
+}
+
+export default App;
